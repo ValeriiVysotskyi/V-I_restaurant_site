a0774c538e89610af5aa66dc7dd6582fd408a632
Valerii Vysotskyi <Valerii.Vysotskyi@nure.ua>
2026-06-01 02:36:33 +0300

    feat(backend) create .env and DbConnect.php

---
+++ app/dal/DbConnect.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+
+class DbConnect
+{
+    private static $connection = null;
+
+    public static function connect()
+    {
+        if (self::$connection === null) {
+
+            $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+            $dotenv->load();
+
+            self::$connection = new PDO(
+                "mysql:host={$_ENV['DB_HOST']};dbname={$_ENV['DB_NAME']};charset=utf8",
+                $_ENV['DB_USER'],
+                $_ENV['DB_PASS']
+            );
+
+            self::$connection->setAttribute(
+                PDO::ATTR_ERRMODE,
+                PDO::ERRMODE_EXCEPTION
+            );
+        }
+
+        return self::$connection;
+    }
+}
+
